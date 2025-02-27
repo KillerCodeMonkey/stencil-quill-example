19e54b5084b04c9adcdc99e3de061f97445951c0
KillerCodeMonkey <bengtler@gmail.com>
2025-02-27 14:09:02 +0100

    chore: init

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,6 +1,6 @@
 import { Component, h } from '@stencil/core';
-import { Router } from "../../";
-import { Route, match } from "stencil-router-v2";
+
+import 'stencil-quill'
 
 @Component({
   tag: 'app-root',
@@ -10,25 +10,9 @@ import { Route, match } from "stencil-router-v2";
 export class AppRoot {
   render() {
     return (
-      <div>
-        <header>
-            <h1 onClick={() => Router.push("/")}>
-              Stencil App Starter
-            </h1>
-        </header>
-
         <main>
-          <Router.Switch>
-            <Route path="/">
-              <app-home />
-            </Route>
-            <Route
-              path={match("/profile/:name")}
-              render={({ name }) => <app-profile name={name} />}
-            />
-          </Router.Switch>
+          <quill-editor></quill-editor>
         </main>
-      </div>
     );
   }
 }
